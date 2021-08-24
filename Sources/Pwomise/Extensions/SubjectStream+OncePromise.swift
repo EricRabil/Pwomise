1649e4056f4729cd314c1bf474f65ca816d612ea
Eric Rabil <ericjrabil@gmail.com>
2021-08-24 15:35:55 -0400

    Add iOS compatbility

--- Sources/Pwomise/Extensions/SubjectStream+OncePromise.swift
+++ Sources/Pwomise/Extensions/SubjectStream+OncePromise.swift
@@ -5,10 +5,12 @@
 //  Created by Eric Rabil on 8/24/21.
 //
 
+#if canImport(Combine)
+
 import Foundation
 import Swexy
 
-@available(macOS 10.15, *)
+@available(macOS 10.15, iOS 13.0, *)
 public extension SubjectStream {
     @discardableResult
     func oncePromise(where valid: @escaping (Element) -> Bool) -> Promise<Element> {
@@ -17,3 +19,5 @@ public extension SubjectStream {
         }
     }
 }
+
+#endif
